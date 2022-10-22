1702e23e554c311a49b6cd964bd9e77fb2df5e97
alexqwinta <82706111+alexqwinta@users.noreply.github.com>
2022-10-22 10:39:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Alexqwinta!</font><br><b>App Version 1.6</b>";
+  const message="<font color='blue'>СloudFunction of Alexqwinta!</font><br><b>App Version 1.7</b>";
   res.status(200).send(message);
 };
